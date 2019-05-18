84820d6d5d76ffcc36353000b17bd211b036150b
jamcry <jamcry@hotmail.com>
2019-05-18 21:58:42 +0300

    Add Event model and associations with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
+  has_many :events, foreign_key: "creator_id"
 end
