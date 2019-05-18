054270bddc5dff94b5b34aad208c2d718c588d89
jamcry <jamcry@hotmail.com>
2019-05-19 02:03:45 +0300

    Complete Event model

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,30 @@
+class EventsController < ApplicationController
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:success] = "Created new event."
+      redirect_to @event
+    else
+      flash[:warning] = "Cannot create event"
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  private
+
+      def event_params
+        params.require(:event).permit(:title)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  get 'events/new'
+
+  get 'events/create'
+
+  get 'events/show'
+
   root 'static_pages#index'
   get 'users/new'
   get 'users/create'
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
+  get  'signup',    to: 'users#new'
+  get  'login',     to: 'sessions#new'
+  post 'login',     to: 'sessions#create'
+  get  'logout',    to: 'sessions#destroy'
+  get  'new_event', to: 'events#new'
+  post 'new_event', to: 'events#create'
+  resources :events
   resources :users
   resources :static_pages
 end
