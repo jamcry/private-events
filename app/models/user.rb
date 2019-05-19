fee97a36b8d5a1b8566fe5f9106fcb3e152c709e
jamcry <jamcry@hotmail.com>
2019-05-19 14:32:05 +0300

    Rename user.events to user.created_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
-  has_many :events, foreign_key: "creator_id"
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
 end
