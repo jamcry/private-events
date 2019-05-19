789a56930f8875db7b1f3a7e878e2f576257b445
jamcry <jamcry@hotmail.com>
2019-05-19 16:16:31 +0300

    Add previous/upcoming events feature to events index

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: "attended_event_id"
   has_many :attendees, through: :attendances
+
+  scope :previous_events, -> { where("date < ?", Time.now)}
+  scope :upcoming_events, -> { where("date > ?", Time.now)}
 end
