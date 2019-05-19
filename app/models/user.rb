f00d438581996309bd7bac09af8f6db2f2ecd93f
jamcry <jamcry@hotmail.com>
2019-05-19 13:36:45 +0300

    Add attendances model (not working)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
-  has_many :events, foreign_key: "creator_id"
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+  has_many :attendances, foreign_key: "attendee_id"
+  has_many :attended_events, through: :attendances, foreign_key: "attended_event_id"
 end
