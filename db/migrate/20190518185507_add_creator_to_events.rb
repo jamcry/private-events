3fcf5737fe96812d09af2f42c9e4a50f0941e5f9
jamcry <jamcry@hotmail.com>
2019-05-21 19:04:09 +0300

    Migration changes for production

--- db/migrate/20190518185507_add_creator_to_events.rb
+++ db/migrate/20190518185507_add_creator_to_events.rb
@@ -1,5 +1,6 @@
 class AddCreatorToEvents < ActiveRecord::Migration[5.1]
   def change
-    add_reference :events, :creator, foreign_key: true
+    #add_reference :events, :creator, foreign_key: true
+    add_column :events, :creator_id, :integer, index: true
   end
 end
